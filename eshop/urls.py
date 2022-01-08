ed16c0058e2a311b0ad4a3de75952aeacdee2d20
Darkneska <84958830+Darkneska@users.noreply.github.com>
2022-01-08 14:39:17 +0100

    prepare css for main page

--- eshop/urls.py
+++ eshop/urls.py
@@ -18,8 +18,12 @@ from django.urls import path
 
 from store import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', views.homepage, name='home'),
     path('admin/', admin.site.urls),
 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
